7cc7c584cbe3c382a6f44bc0df91721fe0df3620
[apalatnik] <[s.palatnik@gmx.de]>
2019-05-15 00:49:34 +0200

    done

---
+++ 03_trials.js
@@ -0,0 +1,62 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    keyPress: [
+        {
+            question: "same or different?",
+            picture: 'mental_rotation_images/1_50_different.jpg',
+            key1: 's',
+            key2: 'd',
+            s: 'same',
+            d: "different",
+            expected:"different",
+            numberOfPic: "1",
+            degreeOfRotation: "50"
+        },
+        {
+          question: "same or different?",
+          picture: "mental_rotation_images/8_150_same.jpg",
+          key1: "s",
+          key2: "d",
+          s: "same",
+          d: "different",
+          expected:"same",
+          numberOfPic: "8",
+          degreeOfRotation: "150"
+        },
+        {
+          question: "same or different?",
+          picture: "mental_rotation_images/13_50_same.jpg",
+          key1: "s",
+          key2: "d",
+          s: "same",
+          d: "different",
+          expected:"same",
+          numberOfPic: "13",
+          degreeOfRotation: "50"
+        },
+        {
+          question: "same or different?",
+          picture: "mental_rotation_images/6_50_different.jpg",
+          key1: "s",
+          key2: "d",
+          s: "same",
+          d: "different",
+          expected:"different",
+          numberOfPic: "6",
+          degreeOfRotation: "50"
+        },
+        {
+          question: "same or different?",
+          picture: "mental_rotation_images/11_150_same.jpg",
+          key1: "s",
+          key2: "d",
+          s: "same",
+          d: "different",
+          expected:"same",
+          numberOfPic: "11",
+          degreeOfRotation: "150"
+        }
+    ]
+};
